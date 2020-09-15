b4c5b93cba57caaa7f7e64ff1c57f373ca454e92
tusqasi <2002.tushar.kuntawar@gmail.com>
2020-09-15 22:17:17 +0530

    solved 14collazteskdf

--- 14Longest_Collatz_sequence.py
+++ 14Longest_Collatz_sequence.py
@@ -1,38 +1,28 @@
 # 14Longest_Collatz_sequence.py
-# unsolved
+# solved
+# 837799
 
 def collatz_nums(n):
+    copy = n
     counter = 1
-    print(int(n), counter)
     while n != 1:
         counter += 1
         if n % 2 == 0:
             n /= 2
         elif n % 2 != 0:
             n = (3 * n) + 1
-        print(counter, int(n))
-    return counter
+    return counter, copy
     
     
-def pow_of_2(n):
-    if n % 2 != 0:
-        return False
-
-    while True:
-        if n == 1:
-            return True
-        elif n % 2 == 0: 
-            n = n // 2
-            continue
-        elif n % 2 != 0:
-            return False
-
 
 
 
 list_of_counters = []
-for i in range(1,1e6):
-    if not pow_of_2(i):
-        continue
+list_of_nums = []
+for i in range(1, int(1e6)):
+    num = collatz_nums(i)
+    list_of_counters.append(num[0])
+    list_of_nums.append(num[1])
 
+lisd = dict()
     
