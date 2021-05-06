958b5ad74525a242a46a0d7a4c4a05ffc11de787
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-05-06 11:39:37 +0530

    update

--- 5_smallest_multiple.py
+++ 5_smallest_multiple.py
@@ -1,12 +1,29 @@
-# 5smallest_multiple.py
-from primefunctions import * 
-from math import sqrt
+from primefunctions import factors
 
-def occurs(n, lst):
-    """How many times does n occur in lst"""
 
-    c = 0
-    for i in lst:
-        if i == n:
-            c += 1
-    return c
+def prime_list(n: int) -> dict:
+    ret = {}
+    facs = factors(n)
+    for i in facs:
+        ret[i] = facs.count(i)
+    return ret
+
+
+def main():
+    max_primes = {}
+    for i in range(1,21):
+        prime_l = prime_list(i)
+        for prime, prime_total in prime_l.items():
+            # print(prime)
+            if not prime in max_primes:
+                max_primes[prime] = prime_total
+            elif prime_total > max_primes[prime]:
+                max_primes[prime] = prime_total
+    
+    prod = 1
+    for prime, prime_total in max_primes.items():
+        prod*=prime ** prime_total
+    print(prod)
+
+if __name__ == "__main__":
+    main()
